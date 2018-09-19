2086c388512dabca8203a5a6b5b1635af0615862
Kolbod <npablow@gmail.com>
2018-09-19 10:28:02 -0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+  "use strict";
+
+  
+
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      autoplay:true,
+    })
+
+
+    // PARALLAX EFFECT
+    $.stellar();
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.navbar-default a, #home a, footer a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });
+
+
+    // WOW ANIMATION
+    new WOW({ mobile: false }).init();
+
+})(jQuery);
