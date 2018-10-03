e53425c3793b2b10e8ec3757d989e6702be4ae3b
Kolbod <43389088+Kolbod@users.noreply.github.com>
2018-10-03 14:08:06 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -50,50 +50,101 @@ URLhashListener:true,
 
   });
 
-  $('.nuno').click(function(){
-  $('.logo img').attr('src',"img/hablacorto.gif");
-  setTimeout(function(){ $('.logo img').attr('src',"img/loop.gif"); }, 4000);
-
-  });
-
-  $('.ndos').click(function(){
-  $('.logo img').attr('src',"img/loop.gif");
-
-  });
-
-  $('.ntres').click(function(){
-  $('.logo img').attr('src',"img/hablacorto.gif");
-  setTimeout(function(){ $('.logo img').attr('src',"img/loop.gif"); }, 4000);
-  });
-
-  $('.ncuatro').click(function(){
-  $('.logo img').attr('src',"img/loop.gif");
-
-  });
-
-  $('.ncinco').click(function(){
-  $('.logo img').attr('src',"img/hablalargo.gif");
-  setTimeout(function(){ $('.logo img').attr('src',"img/loop.gif"); }, 6000);
-  });
 
 
-
-
-
-
-
-
-
-$(".este").on('click', function(){
-  $(".otro").removeClass("active");
-  $(".este").addClass("active");
-  });
 
 
   $(".otro").on('click', function(){
     $(".este").removeClass("active");
     $(".otro").addClass("active");
     });
+
+    $(".este").on('click', function(){
+      $(".otro").removeClass("active");
+      $(".estre").addClass("active");
+      });
+
+
+
+    $(".huergo").mouseover(function(){
+      $("remover").remove("remover");
+      $(".exp-est-img").attr('src',"img/huergo.png");
+      $(".text-block-est").append("<remover><h3>Industrial Institute Luis A. Huergo High School</h3><p><b>Electronic Technician with computer orientation  </b> March 2000 - January 2006</p><p><em>Completed</em></p></remover>");
+
+      });
+
+      $(".huergo").mouseleave(function(){
+        $(".exp-est-img").attr('src',"img/wire.png");
+        $("remover").remove("remover");
+        });
+
+        $(".repa").mouseover(function(){
+          $("remover").remove("remover");
+          $(".exp-est-img").attr('src',"img/repa.png");
+          $(".text-block-exp").append("<remover><h3>Computers Repair and Assembly</h3><p>February 2004 - March 2007</p></remover>");
+
+          });
+
+          $(".repa").mouseleave(function(){
+            $(".exp-est-img").attr('src',"img/wire.png");
+            $("remover").remove("remover");
+                        });
+
+            $(".dv").mouseover(function(){
+              $("remover").remove("remover");
+              $(".exp-est-img").attr('src',"img/dv.png");
+                $(".text-block-est").append("<remover><h3>Leonardo Da Vinci Institute</h3><p><b>Multimedia Designer</b>  March 2006 - March 2009</p><p><em>Completed</em></p><p><b>Animation Cinema Director</b> March 2009 - December 2012</p><p><em>Tesis Pending</em></p></remover>");
+                $(".text-block-exp").append("<remover><h3>Leonardo Da Vinci Institute</h3><p><b>Professor in Multimedial Design Career & Animation Cinema Career</b></p><p><b>Modeling, Rendering, Rigging, Animation, Simulations</b> March 2012 - Current</p><p><em>https://www.davinci.edu.ar/</em></p></remover>");
+                            });
+
+              $(".dv").mouseleave(function(){
+                $(".exp-est-img").attr('src',"img/wire.png");
+                $("remover").remove("remover");
+                });
+
+                $(".lime").mouseover(function(){
+                  $("remover").remove("remover");
+                  $(".exp-est-img").attr('src',"img/lime.png");
+                    $(".text-block-exp").append("<remover><h3>Lime Solutions</h3><p><b>Customer Care, Multimedial Designer, Website developer</b></p><p><b> Graphic Design, Action Script Games, 3D Team Leader</b></p><p><b>Motion Graphics</b> April 2008 - September 2012</p></remover>");
+                  });
+
+                  $(".lime").mouseleave(function(){
+                    $(".exp-est-img").attr('src',"img/wire.png");
+                    $("remover").remove("remover");
+                    });
+
+                    $(".mocap").mouseover(function(){
+                      $("remover").remove("remover");
+                      $(".exp-est-img").attr('src',"img/mocap.png");
+                        $(".text-block-exp").append("<remover><h3>Getmotion</h3><p><b>Coordination, Motion Capture equipment operator</b></p><p><b> Equipment maintenance, Animation clean up</b> March 2012 - Current</p><p><em>https://www.getmotion.tv/</em></p></remover>");
+                      });
+
+                      $(".mocap").mouseleave(function(){
+                        $(".exp-est-img").attr('src',"img/wire.png");
+                        $("remover").remove("remover");
+                        });
+
+                        $(".freelo").mouseover(function(){
+                          $("remover").remove("remover");
+                          $(".exp-est-img").attr('src',"img/freelo.png");
+                            $(".text-block-exp").append("<remover><h3>Freelancer</h3><p><b>Rigging, Animation, Architectural Rendering</b></p><p><b>Character and Props Modeling</b> February 2013 - Current</p></remover>");
+                          });
+
+                          $(".freelo").mouseleave(function(){
+                            $(".exp-est-img").attr('src',"img/wire.png");
+                            $("remover").remove("remover");
+                            });
+
+                            $(".exchange").mouseover(function(){
+                              $("remover").remove("remover");
+                              $(".exp-est-img").attr('src',"img/exchange.png");
+                                $(".text-block-est").append("<remover><h3>Languajes</h3><p><b>Spanish - Native</b></p><p><b>English - Fluent</b><em> TOEFL 276/300, Rotary Club Rotary Exchange Student USA</em> - 2005 </p> </remover>");
+                              });
+
+                              $(".exchange").mouseleave(function(){
+                                $(".exp-est-img").attr('src',"img/wire.png");
+                                $("remover").remove("remover");
+                                });
 
 
 
